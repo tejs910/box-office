07e10e576a1adbf0e9780971e1e36e1798f06329
tej gokani <tbgokani@gmail.com>
2022-09-14 02:40:49 +0530

    added router-dom and routed

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
 import React from "react";
-
+import { Switch, Route } from "react-router-dom";
 function App() {
-  return <div></div>;
+  return (
+    <Switch>
+      <Route exact path="/">
+        This is home page
+      </Route>
+      <Route path="/starred">This is starred page</Route>
+      <Route>Sorry not accepted</Route>
+    </Switch>
+  );
 }
 
 export default App;
