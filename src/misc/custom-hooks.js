b266ec8ae39232a61f946e08a9a2aa9a9e89b18f
tej gokani <tbgokani@gmail.com>
2022-09-20 16:23:28 +0530

    created custom hook for show boomkark by copy paste

---
+++ src/misc/custom-hooks.js
@@ -0,0 +1,27 @@
+import { useEffect, useReducer } from "react";
+
+function showsReducer(prevState,action){
+  switch(action.type){
+    case 'ADD' : {
+      return [...prevState,action.showId]
+    }
+    case 'Remove' : {
+      return prevState.filter((showid)=> showid !== action.showId)
+    }
+    default : return prevState
+  }
+}
+function usePersistedReducer(reducer, initialState, key) {
+  const [state, dispatch] = useReducer(reducer, initialState, (initial) => {
+    const persisted = localStorage.getItem(key);
+    return persisted ? JSON.parse(persisted) : initial;
+  });
+  useEffect(() => {
+    localStorage.setItem(key, JSON.stringify(state));
+  }, [state, key]);
+  return [state, dispatch];
+}
+
+export default function useShows(key = "show") {
+  return usePersistedReducer(showsReducer, []key);
+}
